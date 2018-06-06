22fbd9346fe9d5e5fa751024217a6437f4b90b6f
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-06-06 11:19:21 +0200

    Install ccache on macOS

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 brew update
-brew install smpeg2 libpng freetype sdl2 sdl2_ttf sdl2_image qt5 ffmpeg ninja
+brew install smpeg2 libpng freetype sdl2 sdl2_ttf sdl2_image qt5 ffmpeg ninja ccache
 brew install sdl2_mixer --with-smpeg2
 
+export PATH=/usr/local/opt/ccache/libexec:$PATH
 export CMAKE_PREFIX_PATH="/usr/local/opt/qt5:$CMAKE_PREFIX_PATH"
